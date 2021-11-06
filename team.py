61bf7eb2728a766d51576ca6bac91fbe98d708d5
Mazilius <46424521+Mazilius@users.noreply.github.com>
2021-11-06 22:00:19 +0100

    Update team.py

--- team.py
+++ team.py
@@ -1,29 +1,29 @@
-player_names = []
+names = []
 
 
-while len(player_names) <= 2:
+while len(names) <= 2:
      question = input("Hello, would you like to add players to the team? \nY/N ")
      if question.isdigit():
         print("No numbers!")
         continue
    
      if question.lower() == "yes":
-        player_names = input("Enter the name of the player to add to the team ")
+        names = input("Enter the name of the player to add to the team ")
 
-        if player_names.isdigit():
+        if names.isdigit():
             print("No numbers!")
             continue
 
-        player_names.append(player_names) 
+        names.append(names) 
        
      elif question.lower() == "no":
-        print(player_names)
+        print(names)
         break
    
-print("There are {} players on this team!".format(len(player_names)))
+print("There are {} players on this team!".format(len(names)))
 
 player_number = 1
-for player in player_names:
+for player in names:
     print("Player {}: {}".format(player_number, player))
     player_number += 1
 
@@ -31,10 +31,10 @@ for player in player_names:
 goalkeeper = int(input("Who do you want as goalkeeper? "))
 
 if goalkeeper == 1:
-    print("The goalkeeper is {}".format(player_names[0]))
+    print("The goalkeeper is {}".format(names[0]))
 elif goalkeeper == 2:
-    print("The goalkeeper is {}".format(player_names[1]))
+    print("The goalkeeper is {}".format(names[1]))
 elif goalkeeper == 3:
-    print("The goalkeeper is {}".format(player_names[2]))
+    print("The goalkeeper is {}".format(names[2]))
 else:
     print("Invalid input!")
