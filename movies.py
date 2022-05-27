2ebb239dc6fe7319a7009c1a2e9ec95be17a2365
Mazilius <46424521+Mazilius@users.noreply.github.com>
2022-05-27 04:41:21 +0200

    Added a new list that shows age specific movies

--- movies.py
+++ movies.py
@@ -1,7 +1,9 @@
 import sys as s
 import math as m
+from tabulate import tabulate
 
 #-------------------------------------------------------------------------------------#
+
 print(""" 
 Welcome to python movies!
 There are different age criterias you have to meet in order to use our services
@@ -23,10 +25,10 @@ def welcome():
         elif len(username) <= 3:
             print("Name is too short!")
         else:
-            print(f"Welcome to Python movies, {username}. You will be proceeded to the ticket venue now")
+            print(f"Welcome to Python movies, {username}. You will be proceeded to the available movie and later proceeded to the ticket venue \n")
             break
             
-
+print()
     
 
 """""
@@ -38,21 +40,79 @@ while True:
     except ValueError:
         print("Value is not valid!")
     else:  # If everything is valid with the value then the while loop will run
-        if age <= 13:
+        if age <= 6:
             print(f"You are not old enough! You have {attempt} attempts left")
             attempt -= 1
             if attempt < 0:
                 s.exit("Too many invalid tries!")
-        elif age >= 13 and age <= 18:
+        elif age >= 6 and age <= 18:
             print("Welcome to the movies!")
-            print("You are allowed to watch R15 movies!")
+            print("You are able to watch the following titles!")
             welcome()
             break
         else:
+            print("Welcome to the movies!")
             print("You can watch every available movie!")
             welcome()
             break
          
+
+"""""
+Movie selection
+"""""
+#TODO: Add a list here with different movies, age restriction and how many tickets left
+movie_list = [
+    ["Black Widow", 13, 12],
+    ["Django unchained", 18, 14], 
+    ["Spider-Man: No Way Home", 13, 3],
+    ["Avengers: Endgame", 13, 1],
+    ["Winther on Fire: Ukraine's Fight for Freedom", 15, 17],
+    ["Doctor Strange in the multiverse of madness", 11, 4],
+    ["Fantastic Beasts 3", 11, 20],
+    ["Sonic the Hedgehog 2", 7, 5],
+    ["The Northman", 15, 24],
+    ["Speak No Evil", 18, 7],
+    ["C'mon C'mon", 18, 10],
+    ["The Batman", 11, 4],
+    ["Paris, 13th Distric", 15, 40],
+    ["Morbius", 15, 4]
+]
+
+#TODO: Print the name values out of the nested list
+movie_age_list = []
+
+for movie in movie_list:
+    if age >= movie[1]:
+        movie_age_list.append(movie)
+        
+        
+
+
+print(tabulate(movie_age_list, headers = ["Movie", "PG", "No. Tickets"], tablefmt="fancy_grid"))
+
+
+#TODO: Pick a specific movie from the new list and show the ticket amounts
+print("""
+!Please notice that there are limited amount of tickets for every movie!
+""")
+
+#movie_selection = []
+#while True:
+
+
+
+
+#print("This is your desired movie!")
+#print(tabulate(movie_selection, headers = ["Movie", "PG", "No. Tickets"], tablefmt="fancy_grid"))
+#while True:
+    #selection = input("Please type in your desired movie: ")
+    #if selection not in movie_age_list:
+    #    print("Movie is non existent")
+    #else:
+    #    movie_selection.append(selection) 
+    #    break
+
+
 
 
 """""
@@ -67,7 +127,7 @@ def calculator(tickets):
     return tickets * TICKET_PRICE + SERVICE_CHARGE
 
 while TICKET_AMOUNT >= 1:
-    print(f"There are {TICKET_AMOUNT} remaining and the price is ${TICKET_PRICE} per ticket and ${SERVICE_CHARGE} for the use of service")
+    print(f"There are {TICKET_AMOUNT} tickets remaining and the price is ${TICKET_PRICE} per ticket and ${SERVICE_CHARGE} for the use of service")
     try:
         number_of_tickets = int(input("How many tickets you want? "))
         if number_of_tickets > TICKET_AMOUNT or number_of_tickets == 0:
@@ -90,5 +150,3 @@ while number_of_tickets >= 2:
     total_due = split_price()
     print(f"Each person has to pay ${total_due}")
     break
-
-#TODO: Add a dictoronairy here with different movies, age restriction and how many tickets left
